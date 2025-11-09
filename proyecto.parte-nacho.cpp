66275b680f5885ce1da8efea3c2c4916659be58c
nacho <nachocerrada0306@gmail.com>
2025-11-09 12:47:14 -0400

    segundo commit

--- proyecto.parte-nacho.cpp
+++ proyecto.parte-nacho.cpp
@@ -1,20 +1,70 @@
+#include <iostream>
+#include <random>
+using namespace std;
+
+struct ficha{
+    int izq;
+    int der;
+    ficha* prox;
+};
+
+struct Jugador {
+    int id;
+    ficha* mano;
+    int puntos;
+    Jugador* prox;
+};
+
+struct pozo{
+    ficha* fichapozo;
+    pozo* prox;
+};
+
+struct mesa{
+    ficha* inicio;
+    ficha* fin;
+    int izq;
+    int der;
+};
 
 ficha* CrearFicha(int izq, int der){
     ficha* nueva = new ficha;
     nueva->izq=izq;
     nueva->der=der;
-    nueva->siguiente=nullptr;
+    nueva->prox=nullptr;
     return nueva;
 }
 
-ficha* llenarFicha(ficha*& FichasMezcladas, pozo* Pozo, int NumeroDeJugadores){
-    
-    for(int i=0, i<=6,){
-        i++;
-        for(int j = 0, j <=6){
-            j++;
-            ficha* nueva = CrearFicha(i,j);
-        }
+void ListasFichas(ficha *&inicio, int valorIzq, int valorDer){
+    ficha *nuevo = CrearFicha(valorIzq, valorDer);
+    if (inicio==nullptr){
+        inicio = nuevo;
+    } else{
+        ficha *auxiliar = inicio;
+        while (auxiliar->prox != NULL){
+        auxiliar = auxiliar->prox;
+    }
+    auxiliar->prox = nuevo;
     }
 }
 
+ficha* llenarFicha(){
+    ficha* fichasMezcladas = nullptr;
+    for(int i=0; i<=6; i++){
+        for(int j = i; j <=6;j++){
+            ListasFichas(fichasMezcladas, i, j);
+        }
+    }
+    return fichasMezcladas;
+}
+
+pozo* crearPozo(ficha* fichasMezcladas){
+    pozo* nuevoPozo = new pozo;
+    nuevoPozo->fichapozo = fichasMezcladas;
+    nuevoPozo->prox = nullptr;
+    return nuevoPozo;
+}
+
+Jugador* RepartirFichas(pozo *&pozo, int n, ficha* mano){
+    
+}
